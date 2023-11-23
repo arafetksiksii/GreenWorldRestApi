615d05f297186f2bb8c6c6ad34e44361d21a8aab
Walidbouzouita <125035094+Walidbouzouita@users.noreply.github.com>
2023-11-23 16:58:16 +0100

    tri_des_user_score

--- controllers/user.js
+++ controllers/user.js
@@ -17,9 +17,8 @@ const transporter = nodemailer.createTransport({
 
 //recover all users
 export function getAllUsers(req, res) {
- 
-   
   User.find({})
+    .sort({ score: -1 }) // Ajoutez cette ligne pour trier par score en ordre décroissant
     .then((users) => {
       let userList = users.map((user) => {
         return {
@@ -35,6 +34,7 @@ export function getAllUsers(req, res) {
           isValid: user.isValid,
           imageRes: user.imageRes,
           role: user.role,
+          score: user.score, // Assurez-vous que le modèle User a un champ score
         };
       });
       res.status(200).json(userList);
@@ -42,8 +42,8 @@ export function getAllUsers(req, res) {
     .catch((err) => {
       res.status(500).json({ error: err });
     });
-
 }
+
 // add user
 
 export async function addUser(req, res) {
@@ -206,38 +206,40 @@ export function deleteUserById(req, res) {
 
 
 export function updateScoreById(req, res) {
-  
-
-
-  // Check if the request body contains the "score" attribute
+  // Vérifier si le corps de la requête contient l'attribut "score"
   if (!req.body.score) {
     console.log(req.body.score)
     return res.status(400).json({ message: 'Bad Request - Score is required' });
-    
   }
 
   if (!req.body.id) {
     console.log(req.body.id)
-
     return res.status(400).json({ message: 'Bad Request - id is required' });
   }
 
-  const updatedUserData = {
-    score: req.body.score,
-  };
-
-  User.findByIdAndUpdate(req.body.id, updatedUserData, { new: true })
-    .then((updatedUser) => {
-      if (!updatedUser) {
-        res.status(404).json({ message: 'Utilisateur introuvable' });
-        console.log("ccccc")
-      } else {
-        res.status(200).json(updatedUser);
+  // Trouver l'utilisateur par ID
+  User.findById(req.body.id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
       }
+
+      // Ajouter le nouveau score au score existant
+      const updatedScore = user.score + req.body.score;
+
+      // Mettre à jour le score de l'utilisateur
+      User.findByIdAndUpdate(req.body.id, { score: updatedScore }, { new: true })
+        .then(updatedUser => {
+          res.status(200).json(updatedUser);
+        })
+        .catch(err => {
+          res.status(500).json({ error: err });
+        });
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(500).json({ error: err });
     });
 }
+
 
 
